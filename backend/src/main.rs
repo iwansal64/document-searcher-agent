add8a3187367b0a42c9c87b67b650001df0394e7
Iwansal64 <iwansal64@gmail.com>
2026-09-07 19:23:30 +0700

    <backend>[feat] Test actix-web

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, post};
+
+#[post("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
+
+#[actix_web::main] // Starts the Actix async runtime
+async fn main() -> std::io::Result<()> {
+    // Start the HTTP server
+    HttpServer::new(|| {
+        App::new().service(hello) // Register the macro-routed handlers
+    })
+    .bind(("127.0.0.1", 8080))? // Bind to port 8080
+    .run()
+    .await
 }
